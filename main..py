0dfa557548fc283cd96da400d768a6b56e330205
felipe <lipioliveira25@gmail.com>
2025-07-10 11:28:27 -0300

    Saalvar como Json e Txt

--- main..py
+++ main..py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel
 from PyQt5.QtGui import QPixmap
@@ -26,6 +27,11 @@ class MainWindow(QMainWindow):
         self.open_dir_button.clicked.connect(self.open_dir)
         self.next_img_button.clicked.connect(self.next_img)
         self.prev_img_button.clicked.connect(self.prev_img)
+        self.save_img_button.clicked.connect(self.save_img)
+
+        #Jsons e Txt da imagem
+        self.current_txt_content = ""
+        self.current_json_content = {}
 
     def open_dir(self):
         dir = QFileDialog.getExistingDirectory(self)
@@ -45,6 +51,7 @@ class MainWindow(QMainWindow):
     def show_img(self):
         if self.image_files:
             caminho = os.path.join(self.current_dir, self.image_files[self.current_index])
+            image_filename = self.image_files[self.current_index]
             pixmap = QPixmap(caminho)
             tamanho = self.img_label.size()
             pixmap_scaled = pixmap.scaled(
@@ -53,9 +60,29 @@ class MainWindow(QMainWindow):
                 Qt.SmoothTransformation
             )
             self.img_label.setPixmap(pixmap_scaled)
+
             #Atualiza a seleção da lista para a imagem atual
             index = self.img_list_model.index(self.current_index)
             self.img_list.setCurrentIndex(index)
+
+            #Carrega o .json e .txt associados caso exista
+            base_name, _ = os.path.splitext(image_filename)
+            txt_path = os.path.join(self.current_dir, f"{base_name}.txt")
+            json_path = os.path.join(self.current_dir, f"{base_name}.json")
+            if os.path.exists(json_path):
+                try:
+                    with open(json_path, 'r', encoding='utf-8') as f:
+                        self.current_json_content = json.load(f)
+                        print(self.current_json_content)
+                except Exception as e:
+                    print(f"Erro ao ler JSON: {e}")
+            if os.path.exists(txt_path):
+                try:
+                    with open(txt_path, 'r', encoding='utf-8') as f:
+                        self.current_txt_content = f.read()
+                        print (f"{self.current_txt_content}")
+                except Exception as e:
+                    print(f"Erro ao ler TXT: {e}")
         else:
             self.img_label.clear()
 
@@ -68,6 +95,21 @@ class MainWindow(QMainWindow):
         if self.image_files and self.current_index > 0:
             self.current_index -= 1
             self.show_img()
+
+    def save_img(self):
+        if self.image_files:
+            #Configurando o arquivo
+            image_filename = self.image_files[self.current_index]
+            base_name, _ = os.path.splitext(image_filename)
+            txt_path = os.path.join(self.current_dir, f"{base_name}.txt")
+            json_path = os.path.join(self.current_dir, f"{base_name}.json")
+            content = f"Nome: {image_filename}\n"
+            #.txt
+            with open(txt_path, 'w', encoding='utf-8') as f:
+                f.write(content)
+            #.json
+            with open(json_path, 'w', encoding='utf-8') as f:
+                json.dump({"conteudo": content}, f, indent=4, ensure_ascii=False)
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
